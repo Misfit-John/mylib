b15250d4d47237f472108d02c8f9c26895d38598
zengda <happy_zengda@126.com>
2015-05-04 14:25:14 +0800

    增加通用定义

---
+++ header/JZCommonDefine.h
@@ -0,0 +1,8 @@
+#ifndef __JZ_COMMON_DEFINE_H__
+#define __JZ_COMMON_DEFINE_H__
+typedef unsigned int uint32;
+typedef unsigned char uchar;
+typedef unsigned long long uint64;
+typedef long long int64;
+
+#endif //__JZ_COMMON_DEFINE_H__
