72166d73360e1d614623868821b6064565af80e3
zengda <happy_zengda@126.com>
2015-07-24 18:47:10 +0800

    新的测试用例

--- libTest.cpp
+++ libTest.cpp
@@ -1,69 +1,9 @@
 #   include "JZLogger.h"
 #   include "JZFileUtil.h"
 #   include "JZMacroFunc.h"
-#include "JZCmdParamHandler.h"
-#include <sstream>
-using namespace std;
-
-# if 1
-# endif
-struct mapStr
-{
-	int a;
-	int b;
-	int c;
-	int d;
-	int e;
-};
-class myCmdParm :public JZCmdParamHandler{
-public:
-	myCmdParm (){};
-	virtual ~myCmdParm (){};
-
-	void handle_a_param()
-	{
-		JZWRITE_DEBUG("a param");	
-	}
-	void handle_b_param(const string& arg)
-	{
-		JZWRITE_DEBUG("b param with arg %s", arg.c_str());	
-	}
-
-	void handle_c_var_param(const string& param)
-	{
-		JZWRITE_DEBUG("c parm with arg %s", param.c_str());	
-	}
-	void handleUnknowParam(char opt)
-	{
-		JZWRITE_DEBUG("unknow opt %c", opt);	
-	}
-
-	void handleNonOptParam(const string& param)
-	{
-		JZWRITE_DEBUG("no opt param : %s" ,param.c_str());	
-	}
-	virtual void init()
-	{
-		registerUnknowOptHandler((UnknowOptHandlerPtr)&myCmdParm::handleUnknowParam);		
-		registerNonOptHandler((NonOptHandlerPtr)&myCmdParm::handleNonOptParam);
-		registerOptArgHandlerPtr('b',(OptArgHandlerPtr)&myCmdParm::handle_b_param);		
-		registerOptHandlerPtr('a',(OptHandlerPtr)&myCmdParm::handle_a_param);		
-		registerOptArgHandlerPtr('c',(OptArgHandlerPtr)&myCmdParm::handle_c_var_param);		
-	}
-
-};
-
-
 int main(int argc,char* argv[])
 {
-	char a = 'a';
-	JZLoggerInit();
-	JZSetLoggerLevel(JZ_LOG_ALL);
-	JZSetLoggerOutPutStatue(1,1);
-	JZSetLogFileName("testLog");
-	myCmdParm parm;
-	parm.analyze(argc, argv);
-	
+	JZGetAbsolutePath("./");	
 }
 
 
